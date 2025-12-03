0c2056cd1820e0d770cefd7503fd82bbfd3e1afc
Michael Anthony <manthony@thumbtack.com>
2025-12-03 15:24:03 -0500

    p2 working for d3

--- day3.py
+++ day3.py
@@ -17,18 +17,34 @@ def digit_check(num_str):
                     num_str2 = num_str[ind1+1:len(num_str)]
                     return int(num_str[ind1]) * 10 + int(num_str2[ind2])
 
-
 def part_one(banks):
     score = 0
     for bank in banks.values():
-        print(bank)
+        # print(bank)
         score += digit_check(bank)
     return score
-            
 
+def examine_candidates(digit, nstr):
+    champion = int(nstr)
+    for i in range(len(nstr)):
+        candidate = digit + nstr[:i] + nstr[i+1:]
+        if int(candidate) > champion:
+            champion = int(candidate)
+    return str(champion)
+
+def part_two(banks):
+    score = 0
+    for bank in banks.values():
+        solution = {}
+        first_index = len(bank) - 12
+        solution[first_index] = bank[first_index:]
+        for i in range(first_index -1, -1, -1):
+            solution[i] = examine_candidates(bank[i], solution[i+1])
+        # print(int(solution[0]))
+        score +=int(solution[0])
+    return score
 
 if __name__ == "__main__":
     banks = get_banks()
-    print((banks))
     print(part_one(banks))
-
+    print(part_two(banks))
